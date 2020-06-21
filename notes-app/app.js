b29c894c4bbca057ea61d413284c712bcfb0adaa
Gautam Krishnan (RBEI/BSL5) <tkg1kor@bosch.com>
2020-06-21 11:58:52 +0530

    Second challenge completed

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,2 @@
-const fs = require('fs')
-const { deepStrictEqual } = require('assert')
-//fs.writeFileSync('notes.txt', 'My name is Gautam.')
-
-fs.appendFileSync('notes.txt', 'I live in Palakkad, Kerala. I am 23 years old.')
+const getNotes  = require('./notes.js')
+console.log(getNotes())
