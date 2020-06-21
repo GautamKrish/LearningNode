57ed00e9c77bd3f3ac87442975d48818d974e216
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-06-21 16:30:40 +0530

    Fourth challenge completed

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,41 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes  = require('./notes.js')
 console.log(getNotes())
 console.log(chalk.green.bold.inverse('Hello World!'))
-const command = process.argv[2]
 
-if(command === 'add'){
-    console.log('Adding a note!')
-} else if (command === 'remove'){
-    console.log('Removing a note!')
-}
+// yargs.version('1.1.0')
+
+
+yargs.command({
+    command : 'add',
+    describe : 'Add a note!',
+    handler : function () {
+        console.log('Adding a note!')
+    }
+})
+
+yargs.command({
+    command : 'remove',
+    describe : 'Remove a note',
+    handler : function () {
+        console.log('Removing a note!')
+    }
+})
+
+yargs.command({
+    command : 'list',
+    describe : 'List all the notes',
+    handler : function(){
+        console.log('Listing all notes!')
+    }
+})
+
+yargs.command({
+    command : 'read',
+    describe : 'Read a note',
+    handler : function(){
+        console.log('Reading a note!')
+    }
+})
+console.log(yargs.argv)
