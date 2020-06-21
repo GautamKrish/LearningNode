c60a9ae7c06a00293e4c8befd0912861f618c453
Gautam Krishnan (RBEI/BSL5) <tkg1kor@bosch.com>
2020-06-21 15:43:14 +0530

    Learned how to access command line arguments

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,3 +2,10 @@ const chalk = require('chalk')
 const getNotes  = require('./notes.js')
 console.log(getNotes())
 console.log(chalk.green.bold.inverse('Hello World!'))
+const command = process.argv[2]
+
+if(command === 'add'){
+    console.log('Adding a note!')
+} else if (command === 'remove'){
+    console.log('Removing a note!')
+}
