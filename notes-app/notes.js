c663b01f2b411cdedcf5354afce203b5833a5e8c
Gautam Krishnan (RBEI/BSL5) <tkg1kor@bosch.com>
2020-06-26 23:30:32 +0530

    Used find instead of filter -> performance gain

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -14,8 +14,8 @@ const listNotes = () => {
 
 const addNote = (title, body) => {
     const notes = loadNotes();
-    const duplicateNotes = notes.filter((note) => note.title === title)
-    if(duplicateNotes.length == 0){
+    const duplicateNote = notes.find((note) => note.title === title)
+    if(!duplicateNote){
         notes.push({
             title : title,
             body : body
