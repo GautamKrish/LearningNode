068c39cfe8336e40f610220fa710fbf9a6a32b37
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-06-26 22:58:39 +0530

    Refactored all functions to either ES6 standard or arrow functions

--- notes-app/app.js
+++ notes-app/app.js
@@ -21,7 +21,7 @@ yargs.command({
             type : 'string'
         }
     },
-    handler : function (argv) {
+    handler(argv) {
         console.log('Title : ', argv.title)
         console.log('Description : ', argv.body)
         notes.addNote(argv.title, argv.body)
@@ -38,7 +38,7 @@ yargs.command({
             type : 'string'
         }
     },
-    handler : function (argv) {
+    handler(argv) {
         notes.removeNote(argv.title)
     }
 })
@@ -46,7 +46,7 @@ yargs.command({
 yargs.command({
     command : 'list',
     describe : 'List all the notes',
-    handler : function(){
+    handler() {
         console.log('Listing all notes!')
     }
 })
@@ -54,7 +54,7 @@ yargs.command({
 yargs.command({
     command : 'read',
     describe : 'Read a note',
-    handler : function(){
+    handler() {
         console.log('Reading a note!')
     }
 })
