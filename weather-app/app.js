3c27c72e1fab4fa821d5010175595645afa89c1e
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-06-28 10:26:34 +0530

    Printed a small forecast to the user

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,13 +7,13 @@ const password = process.env.PASSWORD
 const host = process.env.HOSTNAME
 
 
-const url = `http://api.weatherstack.com/current?access_key=${api_key}&query=37.8267,-122.4233`
-
 const options = {
-    proxy : `http://${user_name}:${password}@${host}:8080`
+    url : `http://api.weatherstack.com/current?access_key=${api_key}&query=37.8267,-122.4233&units=s`,
+    proxy : `http://${user_name}:${password}@${host}:8080`,
+    json : true
 }
 
-request(url, options, (_error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request(options, (_error, response) => {
+    const current = response.body.current
+    console.log(`${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees. It feels like ${current.feelslike} though.`)
 })
