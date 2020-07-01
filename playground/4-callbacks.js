2bc37c27d645b1a31be9569bd0026eb6a5afd1ea
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-07-01 09:43:26 +0530

    Understanding callbacks

---
+++ playground/4-callbacks.js
@@ -0,0 +1,36 @@
+console.log('Start')
+
+setTimeout(() => {
+    console.log('Inside setTimeout')
+}, 2000);
+
+
+console.log('Finish')
+
+
+const geocode = (location, callback) => {
+    setTimeout(() => {
+        const data = {
+            latitude : 0,
+            longitude : 0
+        }
+        callback(data)
+    }, 2000);
+}
+
+const data = geocode('philadelphia', (data) => {
+    console.log(data)
+})
+
+const add = (a, b, callback) => {
+    setTimeout(() => {
+        let sum = a + b
+        callback(sum)
+    }, 2000);
+}
+
+add(1, 4, (sum) => {
+    console.log(sum)
+})
+
+
