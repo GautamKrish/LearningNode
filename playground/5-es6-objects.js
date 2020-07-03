3208d0de18469ff873906602bae16987a64af341
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-07-03 15:38:08 +0530

    Learnt a new concept -> Object destructuring

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -9,3 +9,26 @@ const user = {
 }
 
 console.log(user)
+
+//Object destructuring
+const product = {
+    name : 'Red notebook',
+    label : 'stationery',
+    price : 10,
+    stock : 200,
+    rating : 4.8
+}
+
+const {label : productLabel, price, stock, rating} = product
+console.log(productLabel)
+console.log(price)
+console.log(stock)
+console.log(rating)
+
+const productInformation = (type, {label : productLabel, price, rating}) => {
+    console.log(productLabel)
+    console.log(price)
+    console.log(rating)
+}
+
+productInformation('random', product)
