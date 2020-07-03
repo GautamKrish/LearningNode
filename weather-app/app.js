23f4665561af03b1c809dbbbe1d1bd44cc9be5a1
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-07-03 15:03:34 +0530

    made the program flexible to accept address as an input via the command line

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,20 +1,26 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const input = process.argv[2]
+if (input) {
 
-geocode('Boston', (error, geocodeData) => {
-    if(error){
-        console.log('geocode error in app.js')
-       return console.log(error)
-    }
-    debugger
-    console.log(geocodeData)
-    forecast(geocodeData.longitude, geocodeData.latitude, (error, forecastData) => {
-        if(error){
+    geocode(input, (error, geocodeData) => {
+        if (error) {
+            console.log('geocode error in app.js')
             return console.log(error)
         }
-        console.log(geocodeData.location)
-        console.log(forecastData)
+        debugger
+        console.log(geocodeData)
+        forecast(geocodeData.longitude, geocodeData.latitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(geocodeData.location)
+            console.log(forecastData)
+        })
     })
-})
+} else {
+    console.log('Please provide an address!')
+}
+
 
