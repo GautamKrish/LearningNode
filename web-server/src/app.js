f84837b4b2009a06ca2ba7da5f546142a1a9b4ae
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-07-03 22:58:43 +0530

    added about and weather route to the application

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -10,6 +10,13 @@ app.get('/help', (req, res) => {
     res.send('Welcome to help page!')
 })
 
+app.get('/about', (req, res) => {
+    res.send('Welcome to about page!')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('Welcome to weather page!')
+})
 
 app.listen(3000, () => {
     console.log('Server is up and running')
