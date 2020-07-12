2a5b2544eeb8ebcfd5e51d4df32740a240bb35fd
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-07-12 11:51:31 +0530

    client side fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,7 @@
 console.log('Inside client side javascript file')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) =>{
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
