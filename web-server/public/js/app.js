f75e1fc6bd28aac30d118c6458d1d2f45248a6c6
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-07-12 12:05:19 +0530

    implemented fetch for weather app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,7 +1,11 @@
 console.log('Inside client side javascript file')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) =>{
+fetch('http://api.weatherstack.com/current?access_key=9fbea518ac28ae59ce55a830d5ec8d6c&query=42.3605,-71.05965&units=f').then((response) => {
     response.json().then((data) => {
-        console.log(data)
+        if(data.error){
+            return console.log(data.error)
+        }
+        console.log(data.location)
+        console.log(data.current.weather_descriptions[0])
     })
 })
