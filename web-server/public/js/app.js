e330f20a0438f58664a7a6203bf14f16618cb4dd
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-07-13 15:16:40 +0530

    used input value to get weather

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,20 @@
 console.log('Inside client side javascript file')
-
-fetch('http://api.weatherstack.com/current?access_key=9fbea518ac28ae59ce55a830d5ec8d6c&query=42.3605,-71.05965&units=f').then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            return console.log(data.error)
-        }
-        console.log(data.location)
-        console.log(data.current.weather_descriptions[0])
+const fetchForecast = (location) => {
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return console.log(data.error)
+            }
+            console.log(data)
+        })
     })
+}
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+console.log(weatherForm)
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    fetchForecast(location)
 })
