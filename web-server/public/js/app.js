1948b6c4786554136f0f117391ccf602f0067989
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2020-07-13 15:36:32 +0530

    wired up the weather-app user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,20 +1,26 @@
 console.log('Inside client side javascript file')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
 const fetchForecast = (location) => {
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                return console.log(data.error)
+                return messageOne.textContent = data.error
             }
-            console.log(data)
+            messageOne.textContent = `The forecast for ${data.place_name} is ${data.summary}.`
+            messageTwo.textContent = `Currently the temperature is ${data.temperature} degree F, but it feels like ${data.feelsLike} degree F`
         })
     })
 }
 
+
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-console.log(weatherForm)
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
+    messageOne.textContent = 'Loading....'
     fetchForecast(location)
 })
